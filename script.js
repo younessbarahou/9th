911d383ab5e6c8ca7a61b4ee4f21c728ffdad5d0
younessbarahou028@gmail.com <younessbarahou028@gmail.com>
2024-09-21 20:36:18 +0100

    this is an update for the javascript , using the foreach function to optimize the performance with less lines in the code

--- script.js
+++ script.js
@@ -1,43 +1,20 @@
-var value;
-var statuss ;
-var allstatus;
-var a = document.getElementById('uno');
-var b = document.getElementById('dos');
-var c = document.getElementById('tres');
-var d = document.getElementById('quatro');
-var e = document.getElementById('cinco');
-function activate1() {
-     console.log("You clicked : 1");
-     value = 1;
-     return  statuss=true;
-}
-function activate2() {
-     console.log("You Clicked : 2");
-     value = 2;
-    return statuss =true;
-}
-function activate3() {
-     console.log("You Clicked : 3");
-     value = 3;
-    return statuss =true;
-}
-function activate4() {
-     console.log("You Clicked : 4");
-     value = 4;
-    return statuss =true;
-}
-function activate5() {
-    console.log("You Clicked : 5");
-    value = 5;
-    return statuss =true;
-}
-function submit(){
-    sessionStorage.setItem("value",value);
-    if (statuss===true){
-        allstatus === true;
-        window.open("submit.html","_self");
+let value = 0;
+let buttons = document.querySelectorAll("div.numbers button");
+buttons.forEach(function(button, index) {
+        button.onclick = function() {
+        value = index + 1;
+          console.log("You clicked button at index:", value);
+        };
+      });
+document.getElementById('submit').onclick = () =>
+{
+    if (value != 0)
+    {
+        sessionStorage.setItem("value",value);
+        window.open("submit.html" , "_self");
     }
-    else{
+    else
+    {
         document.getElementById('empty').style.display= "block";
         console.error("You should click a value");
     }
